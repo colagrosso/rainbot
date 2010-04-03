dcd09128544268ff899634c962076a2d52ec57be
Mike Colagrosso <mike@colagrosso.net>
2010-04-03 14:57:49 -0600

    Docstring updates.

--- rainbot.py
+++ rainbot.py
@@ -8,12 +8,18 @@ SHELVE_NAME = "rainbot-shelve"
 
 import u3
 
+# strftime format
 TIME_FORMAT = "%a, %m/%d/%Y %l:%M %p"
 
-# Set all the EIO and CIO to output high
+# Set all the EIO and CIO to output high.
+# Remember setting these lines high turns the relays off.
 ALL_OFF_COMMAND = [u3.PortDirWrite(Direction = [0, 0xff, 0xff], WriteMask = [0, 0xff, 0xff]),
                    u3.PortStateWrite(State =   [0, 0xff, 0xff], WriteMask = [0, 0xff, 0xff]) ]
 
+# The keys are the sprinkler zones, the IO nums are the 
+# LabJackPython numbers for the U3.
+# My RB12 is upside-down, so CIO3 is at the top and 
+# EIO0 is at the bottom.
 ZONE_TO_IONUM =  { 1  : 19,     # CIO3
                    2  : 18,
                    3  : 17,
@@ -27,6 +33,7 @@ ZONE_TO_IONUM =  { 1  : 19,     # CIO3
                    11 : 9,
                    12 : 8  }    # EIO0
 
+# We're going to change these all over the place anyway.
 DEFAULT_RUN_TIMES_MINUTES = { 1  : 7,
                               2  : 7,
                               3  : 7,
@@ -40,8 +47,11 @@ DEFAULT_RUN_TIMES_MINUTES = { 1  : 7,
                               11 : 7,
                               12 : 7 }
 
+# I have 12 zones. I know.
 ZONE_STRING_LIST = [ str(i) for i in range(1, 13) ]
 
+# You could configure these to your heart's content on the 
+# old controller. I'm fine with these.
 START_HOUR = 4
 INCREMENT_DAY = 2
 
@@ -57,16 +67,22 @@ class RainBotProtocol(MessageProtocol):
         self.scheduler.shelveConfig.close()
 
     def setStatus(self, statusText, show = None):
+        """
+        Send a presence with statusText. show is one of the allowed strings, like 'xa'.
+        """
         self.send(AvailablePresence(statuses = {None: statusText}, show = show))
 
     def sendText(self, responseText):
+        """
+        Sends responseText in a message to whoever sent RainBot a message last.
+        """
         reply = self._blankMessage()
         reply.addElement("body", content=responseText)        
         self.send(reply)
 
     def onMessage(self, msg):
         #print "Got a message", msg.toXml()
-        self.lastFrom = msg["from"]
+        self.lastFrom = msg["from"] # Save this to send a reply.
 
         if msg["type"] == 'chat' and hasattr(msg, "body") and msg.body != None:
             msgTokens = str(msg.body).split()
@@ -90,7 +106,7 @@ class RainBotProtocol(MessageProtocol):
                     self.handleLast(msgTokens)
                 elif msgCommand == "will" or msgCommand == "w":
                     self.handleWill(msgTokens)                    
-                elif msgCommand == "help" or msgCommand == "h":
+                elif msgCommand == "help" or msgCommand == "h" or msgCommand == "?":
                     self.handleHelp(msgTokens)
                 elif msgCommand in ZONE_STRING_LIST:
                     self.handleZone(msgTokens)
@@ -99,6 +115,10 @@ class RainBotProtocol(MessageProtocol):
             
 
     def handleOn(self, msgTokens):
+        """
+        After running the scheduler's turn on, also send the status 
+        string, because it can be quite long.
+        """
         responseText = "Turning on"
         self.sendText(responseText)
         statusString = self.scheduler.turnOn()
@@ -107,6 +127,9 @@ class RainBotProtocol(MessageProtocol):
         self.sendText(statusString)
 
     def handleOff(self, msgTokens):
+        """
+        Shut it down.
+        """        
         responseText = "Turning off"
         self.sendText(responseText)
         self.scheduler.turnOff()
@@ -114,6 +137,14 @@ class RainBotProtocol(MessageProtocol):
         self.sendText(responseText)
 
     def handlePause(self, msgTokens):
+        """
+        No arguments pauses for one day. The argument can be negative (and zero), but
+        it can't cause the next time to go in the past, so -1 is probably the
+        only negative number you can use.
+        
+        If you call this function again, it will throw out the old pause and
+        calculate a new pause based on when it would have run.
+        """
         pauseDays = 1
         if len(msgTokens) > 1:
             try:
@@ -129,11 +160,17 @@ class RainBotProtocol(MessageProtocol):
         self.sendText(statusString)
 
     def handleRun(self, msgTokens):
+        """
+        Go time. It's going to get wet.
+        """
         responseText = "Running"
         self.sendText(responseText)
         self.scheduler.runProgram()
 
     def handleStop(self, msgTokens):
+        """
+        Ack, I'm all wet. Stop.
+        """
         responseText = "Stopping"
         self.sendText(responseText)
         self.d.getFeedback(ALL_OFF_COMMAND)
@@ -142,6 +179,17 @@ class RainBotProtocol(MessageProtocol):
         self.setStatus(responseText)
 
     def handleTimes(self, msgTokens):
+        """
+        This is a lot of work for one function, but there are a lot of ways to
+        set the time.
+
+        times 7      # All zones to 7 minutes
+        times 12 4   # Zone 12 to 4 minutes
+        times *2     # Double all watering times
+        times *0.5   # Halve all watering times
+        times +2     # Pad times by 2 minutes
+        times +-3    # Down by 3 minutes (I know)
+        """
         runTimeDict = self.scheduler.shelveConfig["runTimesMinutes"]
         if len(msgTokens) == 2:
             try:
@@ -178,18 +226,26 @@ class RainBotProtocol(MessageProtocol):
         self.sendText(responseText)
 
     def handleQuit(self, msgTokens):
+        """
+        Exit. Hopefully you've got monit or something to start you back up.
+        """
         responseText = "Quitting"
         self.sendText(responseText)
         reactor.callLater(1, reactor.stop)
 
     def handleLast(self, msgTokens):
+        """
+        This is in the status, but I want to have it sent to me anyway.
+        """
         responseText = self.scheduler.lastRunStatusString
         self.sendText(responseText)
 
     def handleWill(self, msgTokens):
+        """
+        This is at the end of the status, where sometimes I can't read it.
+        """
         responseText = self.scheduler.willRunStatusString
         self.sendText(responseText)
-
 
     def handleHelp(self, msgTokens):
         responseText = "Commands:\n"
@@ -210,8 +266,13 @@ class RainBotProtocol(MessageProtocol):
         responseText += "\n"
         self.sendText(responseText)
 
-
     def handleZone(self, msgTokens):
+        """
+        You can pass a custom run time, as in
+        
+        12     # Run zone 12 for its set time
+        12 1   # Run zone 12 for 1 minute
+        """
         zoneStr = msgTokens[0]
         customRunTime = None
         if len(msgTokens) > 1:
@@ -233,16 +294,15 @@ class RainBotProtocol(MessageProtocol):
         else:
             responseText = "Unknown zone: " + zoneStr
             self.sendText(responseText)
-            
 
     def handleUnknownCommand(self, msgTokens):
         responseText = "Unknown command: " + msgTokens[0]
         self.sendText(responseText)
 
-
     def _blankMessage(self):            
         """
-        Just add body
+        Just add body, as in
+            reply = self._blankMessage()
             reply.addElement("body", content=responseText)
         """
         reply = domish.Element((None, "message"))
@@ -251,7 +311,10 @@ class RainBotProtocol(MessageProtocol):
         reply["type"] = 'chat'
         return reply
 
-class SchedulerState(object):                 
+class SchedulerState(object):
+    """
+    Yay, state machines.
+    """
     SCHEDULER_OFF, SCHEDULER_ON, SCHEDULER_RUNNING_SCHEDULED, SCHEDULER_RUNNING_MANUAL = range(4)
 
 class Scheduler(object):
@@ -263,12 +326,18 @@ class Scheduler(object):
         self.lastRunStatusString = ""
         self.willRunStatusString = ""
         self.shelveConfig = shelve.open(SHELVE_NAME)
+
+
+        # Check the shelve dictionary for
+        #     lastRun : Last run time
+        #     onState : Whether the sprinklers start on
+        #     runTimesMinutes : Dictionary of how long to run each zone
+        # I call .sync() on it after assigning to it because I'm just that way
         try:
             self.lastRunStatusString = "Last run: " + self.shelveConfig["lastRun"].strftime(TIME_FORMAT) + ". "
         except:
             self.lastRunStatusString = "Last run: unknown. "
-
-        # Check the shelve dictionary
+            self.shelveConfig["lastRun"] = datetime.now()
         try:
             self.shelveConfig["onState"]
         except:
@@ -286,22 +355,31 @@ class Scheduler(object):
             self.turnOff()
 
     def scheduleNextRun(self, pauseDelay = None):
+        """
+        The optional pauseDelay is a timedelta of extra time to add on the 
+        time to run next.
+        """
+        # We are definitely on
         self.shelveConfig["onState"] = True
         self.shelveConfig.sync()
         self.state = SchedulerState.SCHEDULER_ON
+
+        # Cancel any previously scheduled runs
         if self.nextScheduledRun:
             self.nextScheduledRun.cancel()
             self.nextScheduledRun = None
+        
+        # Calculate number of seconds till it's time to run
         now = datetime.now()
         lastRunTime = self.shelveConfig["lastRun"]
-        timeToRun = lastRunTime.replace(hour = START_HOUR, minute = 0, second = 0)
+        timeToRun = lastRunTime.replace(hour = START_HOUR, minute = 0, second = 0) # Love that .replace() from datetime
         while lastRunTime > timeToRun or now > timeToRun:
             timeToRun += timedelta(days = INCREMENT_DAY)
-
         if pauseDelay:
             timeToRun += pauseDelay
         timeTillRun = timeToRun - now
         timeTillRunSeconds = _td_to_seconds(timeTillRun)
+
         self.nextScheduledRun = reactor.callLater(timeTillRunSeconds, self.runProgram)
         self.willRunStatusString = "Will run: " + timeToRun.strftime(TIME_FORMAT) + "."
         try:
@@ -314,6 +392,9 @@ class Scheduler(object):
         return self.scheduleNextRun()
 
     def turnOff(self):
+        """
+        Set the off state, turn off all zones, and set the red light.
+        """
         self.shelveConfig["onState"] = False
         self.shelveConfig.sync()
         self.state = SchedulerState.SCHEDULER_OFF
@@ -325,12 +406,25 @@ class Scheduler(object):
         self.im.setStatus("Sprinklers off", show = "xa") # Red light in the status
 
     def pauseDays(self, numDays):
+        """
+        Reschedule the next run with an extra pause of numDays.
+        
+        Doesn't do anything if the sprinklers are off.
+        """
         if self.state != SchedulerState.SCHEDULER_OFF:
             return self.scheduleNextRun(timedelta(days = numDays))
         else:
             return "Not pausing because sprinklers are off."
 
     def runProgram(self, manual = False):
+        """
+        Run all the zones.
+        
+        If run in manual mode, don't mess with the next scheduled run.
+        
+        If run in regularly scheduled mode, schedule the next run when
+        all the zones are done.
+        """
         if manual:
             self.state = SchedulerState.SCHEDULER_RUNNING_MANUAL
         else:
@@ -339,6 +433,17 @@ class Scheduler(object):
         self.runZone(1, singleZone = False)
 
     def runZone(self, zone, singleZone = False, customRunTime = None):
+        """
+        Run one zone, zone.
+        
+        If running as singleZone, end of story. If not, schedule the next
+        zone after this.
+        
+        Pass in customRunTime (minutes) or use the configured time.
+
+        Returns the number of minutes it will run for so it can be
+        sent as a response message.
+        """
         self.im.setStatus("Running zone: " + str(zone))
         try:
             thisRunTimeSeconds = 60 * self.shelveConfig["runTimesMinutes"][zone]
@@ -354,14 +459,22 @@ class Scheduler(object):
             self.im.setStatus("Couldn't run zone: " + str(zone) + ". Exception: " + str(e))
 
     def runNextZone(self, zone):
+        """
+        Breathe for a second, then run the next zone.
+        """
         self.im.setStatus("Finished zone: " + str(zone))
-        if zone < 12:
+        if zone < len(ZONE_STRING_LIST):
             nextZone = zone + 1
             reactor.callLater(1, self.runZone, nextZone, singleZone = False)
         else:
             reactor.callLater(1, self.ranLastZone)
     
     def ranLastZone(self):
+        """
+        Another successful watering. I can see the grass greener already.
+        
+        Shut everything down, make some notes, and schedule another run if needed.
+        """
         self.turnOffAllZones()
         if self.state == SchedulerState.SCHEDULER_RUNNING_SCHEDULED:
             self.scheduleNextRun()
@@ -372,12 +485,19 @@ class Scheduler(object):
         self.im.setStatus(self.lastRunStatusString + self.willRunStatusString)
 
     def turnOnZone(self, zone):
+        """
+        Use the LabJackPython BitStateWrite to change a single EIO or CI0.
+        
+        Remember setting the line low turns the relay on.
+        """
         self.d.getFeedback(ALL_OFF_COMMAND)
         self.d.getFeedback(u3.BitStateWrite(ZONE_TO_IONUM[zone], 0))
 
     def turnOffAllZones(self):
+        """
+        An all-around handy thing to do.
+        """
         self.d.getFeedback(ALL_OFF_COMMAND)
-
 
 def _td_to_seconds(td):
     '''Convert a timedelta to seconds'''
